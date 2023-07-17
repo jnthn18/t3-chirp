c4fd0933e3d574aadebc3bc062a7943b6faf137b
Jonathan More <jnthn18@gmail.com>
2023-07-16 21:17:37 -0600

    profile image

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 import type { GetStaticPropsContext, NextPage } from "next";
 import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -16,7 +17,20 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 border-slate-400 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
